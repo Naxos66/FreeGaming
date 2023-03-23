1365e7f4786fe434bebe2f17db298f0437a0303f
kouci <mansourakociela@gmail.com>
2023-03-23 14:49:35 +0100

    header init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main.component';
